1396d071fe2e4d770efa554fa92bfd2242024721
popomon <popomonclub@gmail.com>
2023-01-21 19:26:12 +0900

    ci: Added path alias mapping

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,30 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        alias: {
+          '@': './src',
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@hooks': './src/hooks',
+          '@lib': './src/lib',
+          '@ui': './src/ui',
+        },
+      },
+    ],
+  ],
 };

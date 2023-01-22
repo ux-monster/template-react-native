c73a27ef3a7f2e0f0e6cb54d91d337ec3c3d40b1
popomon <popomonclub@gmail.com>
2023-01-22 21:56:54 +0900

    fix: Added DraggableListView component empty file

---
+++ src/components/DraggableListView.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View} from 'react-native';
+
+type Props = {};
+
+const DraggableListView = (props: Props) => {
+  return <View>DraggableListView</View>;
+};
+
+export default DraggableListView;

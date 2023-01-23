02cadc096e1221097c2d480319849ca5a765e9fe
popomon <popomonclub@gmail.com>
2023-01-23 22:25:12 +0900

    feat: Added design snack sample code

---
+++ design-system/src/snacks/Button.snack.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import SnackContainer from '../components/SnackContainer';
+
+const Button = () => {
+  return <SnackContainer id="@hongjisung/uxui_case_study_002" />;
+};
+
+export default Button;

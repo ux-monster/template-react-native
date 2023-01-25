c6c6ebb2a398b010fc4ec65678b4dea608d51107
popomon <popomonclub@gmail.com>
2023-01-25 22:49:28 +0900

    fix: Removed design system

--- design-system/src/snacks/Button.snack.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import SnackContainer from '../components/SnackContainer';
-
-const Button = () => {
-  return <SnackContainer id="@hongjisung/uxui_case_study_002" />;
-};
-
-export default Button;
